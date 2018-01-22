fb8632fa0314674e8b93b40da322a3d38b315db1
aniket wadkar <aniket@vedaitconsulting.co.uk>
2018-01-22 23:03:15 +0000

    Initial Commit

---
+++ src/main/java/com/github/aniketwadkar78/App.java
@@ -0,0 +1,15 @@
+package com.github.aniketwadkar78;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        System.out.println("Maven Central Welcome");
+    }
+}
